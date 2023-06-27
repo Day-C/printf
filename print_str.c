cdf0e94aace13e139f87bd00d395713fba58bc3a
Day-C <kongranalanyuy@gmail.com>
2023-06-27 15:14:00 -0700

    check7

--- print_str.c
+++ print_str.c
@@ -7,8 +7,16 @@
 void prt_str(char *str)
 {
 	int i = 0;
-
-	while (str[i])
+	if (str == NULL)
+	{
+		_putchar('(');
+		_putchar('n');
+		_putchar('u');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+	}
+	while (str[i] !=  '\0')
 	{
 		_putchar(str[i]);
 		i++;
